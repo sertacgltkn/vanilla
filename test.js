eec37adaa19ca27bc4f57ec16d764ffecdd49d27
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-06 17:04:26 +0300

    primitive data types

---
+++ test.js
@@ -0,0 +1,84 @@
+// Primitive data types
+// Number
+// String 
+// Boolean
+// BigInt x > 1.79
+// Null : empty
+// Undefined : not defined
+// Symbol : unique string
+
+// TODO >>>> VARIABLE
+
+var hello = 1
+hello = 5
+var exit = "sadasdasd"
+
+console.log(hello, exit)   // ! terminal >>> node test.js 
+
+
+// TODO >>>> STRİNG 
+
+var name = "Mehmet"
+var surname = "Kaya"
+var age = 25
+
+console.log(name + " " + surname + " " + age)  // ! terminal >>> node test.js
+
+
+boolean = false
+console.log(boolean)
+
+
+// TODO >>>> SYMBOL
+
+test2 = Symbol("test2")
+
+console.log(test2)
+
+
+// TODO >>>> NULL
+
+var test3 = null
+console.log(test3)
+
+// TODO >>>> UNDEFINED
+
+var test4
+console.log(test4)
+
+// TODO >>>> TYPEOF
+
+console.log(typeof test2)  //*! >>>> type of number,boolean, undefined, null, string, symbol
+
+
+
+// TODO >>>>>> A+B 
+
+var a = 5
+var b = 10
+var c = `${a} ${b} merhabalar`
+
+console.log(a+" "+b, c)
+
+
+// TODO >>>>> STRING INDEX [0]
+
+var name = "Mehmet"
+console.log(name[0]) 
+
+console.log(name.charAt(2))
+
+// TODO >>>>> STRING LENGTH
+
+console.log(name.length)
+
+
+// TODO >>>>> STRING indexof (first character)
+
+console.log(name.indexOf("e", 5)) // start of 5
+
+
+// TODO >>>>> STRING lastIndexOf (last character)
+
+console.log(name.lastIndexOf("e"))
+
