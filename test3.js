2926c61a4e8254b73687a9c273b6371200875eda
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-12 12:02:24 +0300

    convert

---
+++ test3.js
@@ -0,0 +1,36 @@
+// string to integer
+var a = "123.2";
+var b = parseInt(a);
+console.log(b); // 123
+
+// string to float
+var a = "123.2";
+var b = parseFloat(a);
+console.log(b); // 123.2
+
+// integer to string
+var a = 123;
+var b = a.toString();
+console.log(b); // "123"
+
+// float to string
+var a = 123.2;
+var b = a.toString();
+console.log(b); // "123.2"
+
+// float to integer
+var a = 123.2;
+var b = parseInt(a);
+console.log(b); // 123
+
+// integer to float
+var a = null;
+var b = parseFloat(a);
+console.log(b); // null
+
+
+
+
+
+
+
