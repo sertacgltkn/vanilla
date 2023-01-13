ddfb6dcd331178b7107c8c56f2855a5d7d344edf
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-13 15:22:35 +0300

    operators

---
+++ test4.js
@@ -0,0 +1,49 @@
+// Operators
+
+var a = "1";
+
+console.log(a==1) // true
+
+console.log(a===1) // false
+
+console.log(a!=1) // false
+
+console.log(a!==1) // true
+
+console.log(a>1) // false
+
+console.log(a<1) // false
+
+console.log(a>=1) // true
+
+console.log(a<=1) // true
+
+console.log(a&&1) // 1
+
+console.log(a||1) // 1
+
+console.log(!a) // false
+
+console.log(a&1) // 1
+
+console.log(a|1) // 1
+
+console.log(a^1) // 0
+
+console.log(a<<1) // 2
+
+console.log(a>>1) // 0
+
+console.log(a>>>1) // 0
+
+console.log(a+=1) // 2
+
+console.log(a-=1) // 1
+
+console.log(a*=1) // 1
+
+console.log(a/=1) // 1
+
+console.log(a%=1) // 0
+
+console.log(a&=1) // 1
