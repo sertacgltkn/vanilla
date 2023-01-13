5627ed661bb06eec59e5da3402b793302f32658e
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-13 15:32:46 +0300

    condition

--- test4.js
+++ test4.js
@@ -47,3 +47,23 @@ console.log(a/=1) // 1
 console.log(a%=1) // 0
 
 console.log(a&=1) // 1
+
+
+var condition1 = 5 > 3
+var condition2 = 6 > 3
+
+console.log(condition1 && condition2) // true
+
+
+var condition1 = 5 > 3
+var condition2 = 6 < 3
+
+console.log(condition1 && condition2) // false
+
+
+var condition12 = 4 > 3
+var condition22 = 2 < 3
+
+console.log(condition12 || condition22) // true
+
+
