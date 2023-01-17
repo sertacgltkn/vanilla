18b670da7b284ee521e886eeee46a674d43794b6
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-17 11:29:12 +0300

    slice method

---
+++ test7.js
@@ -0,0 +1,10 @@
+var var1 = "macbook".slice(0, 3);
+var var2 = "notebook".slice(0, 4);
+var var3 = "iphone".slice(1, 6);
+
+var result = `${var1}${var2}${var3}`.toUpperCase()
+console.log(result);
+
+console.log(var1, var2, var3);
+
+
