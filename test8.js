10140fe94e085d7b52db69a87e02e4e84cf66eb2
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-17 11:47:28 +0300

    else if ex's

---
+++ test8.js
@@ -0,0 +1,7 @@
+var num = "521"
+
+if (num % 2 == 0  ) response = `${num} is even`
+else if (num % 2 == 1) response = `${num} is odd`
+else response = `${num} is not a number`
+
+console.log(response)
