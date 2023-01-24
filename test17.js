09af311528f2a3b13fdf82828225b40673c72e7e
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-24 14:57:59 +0300

    do while loop

---
+++ test17.js
@@ -0,0 +1,22 @@
+// do while loop
+var i = 0;
+do {
+    console.log("Hello World");
+    i++
+} while ( i < 5)
+
+// examples
+var i = [1, 5]
+do {
+    var random = Math.floor(Math.random()*10)
+    console.log(random)
+} while (i.includes(random))
+
+i.push(random)
+
+console.log(random)
+
+
+
+
+
